b0c40e72c2f2fbdfbbed5fa52ebc441fdc88b2b1
ianaf1 <79218845+ianaf1@users.noreply.github.com>
2021-07-11 12:52:19 +0700

    Update user.php

--- admin/mod_user/user.php
+++ admin/mod_user/user.php
@@ -72,7 +72,9 @@
 																					<select class="form-control" name="level" id="level" required>
 																						<option value="">Pilih Level</option>
 																						<option value="admin">Administrator</option>
-																						
+																						<option value="bendahara">Bendahara</option>
+																						<option value="kurikulum">Kurikulum</option>
+																						<option value="Staff">Staff</option>
 																					</select>
 																				</div>
 																				<div class="form-group">
